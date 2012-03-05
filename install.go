cfd54f8bcc57a04433d393cd219617ca9b9363c9
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-05 03:17:52 -0800

    Local install packages

--- install.go
+++ install.go
@@ -1,6 +1,8 @@
 package main
 
 import "flag"
+import "os"
+import "path/filepath"
 
 func (gpkg *Gpkg) install() {
 	logger := gpkg.logger
@@ -8,7 +10,14 @@ func (gpkg *Gpkg) install() {
 	pkgname := readCommand()
 	if pkgname == "" {
 		logger.Fatal("Please specify package name")
+	} else if pkgname == "." {
+		wd, _ := os.Getwd()
+		p := gvm.NewPackage(filepath.Base(wd), "")
+		p.source = wd
+		p.Install()
+		return
 	}
+
 	version := flag.String("version", "", "Package version to install")
 	flag.Parse()
 	if *version == "" {
