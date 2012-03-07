07399d415a26abe125157c6da62fb893977783ac
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-07 13:37:51 -0800

    Error handling updates

--- package.go
+++ package.go
@@ -14,7 +14,6 @@ type Package struct {
 	tag string
 	version *versions.Version
 	source string
-	src string
 	tmpdir string
 	logger *Logger
 	deps map[string]*Package
@@ -24,8 +23,7 @@ func (p *Package) String() string {
 	return "   root: " + p.root + "\n" +
 		"   name: " + p.name + "\n" +
 		"    tag: " + p.tag + "\n" +
-		" source: " + p.source + "\n" +
-		"    src: " + p.src + "\n"
+		" source: " + p.source + "\n"
 }
 
 func (p *Package) GetVersions() []string {
@@ -116,10 +114,6 @@ func (p *Package) Get() bool {
 		}
 	}	
 
-	p.src = filepath.Join(p.root, p.tag, "src")
-	os.MkdirAll(filepath.Join(p.root, p.tag), 0775)
-	FileCopy(tmp_src_dir, p.src)
-
 	return true
 }
 
@@ -149,11 +143,12 @@ func (p *Package) LoadImport(dep *Package, dir string) {
 }
 
 func (p *Package) LoadImports(dir string) bool {
-	//data, err := ioutil.ReadFile(filepath.Join(p.src, p.name, "manifest"))
+	tmp_src_dir := filepath.Join(p.tmpdir, p.name, "src")
+	//data, err := ioutil.ReadFile(filepath.Join(tmp_src_dir, p.name, "manifest"))
 	//if err != nil {
-		data, err := ioutil.ReadFile(filepath.Join(p.src, p.name, "Package.gvm"))
+		data, err := ioutil.ReadFile(filepath.Join(tmp_src_dir, p.name, "Package.gvm"))
 		if err != nil {
-			p.logger.Debug("No dependencies found")
+			p.logger.Debug(" * No dependencies found")
 			return true
 		}
 	//}
@@ -213,6 +208,7 @@ func (p *Package) PrettyLog(buf[] byte) string {
 func (p *Package) Build() bool {
 	tmp_build_dir := filepath.Join(p.tmpdir, p.name, "build")
 	tmp_import_dir := filepath.Join(p.tmpdir, p.name, "import")
+	tmp_src_dir := filepath.Join(p.tmpdir, p.name, "src")
 
 	if !p.LoadImports(tmp_import_dir) {
 		p.logger.Error("Failed to load imports")
@@ -221,7 +217,7 @@ func (p *Package) Build() bool {
 
 	p.logger.Debug(" * Building", p.name, p.tag)
 
-	os.Chdir(filepath.Join(p.src, p.name))
+	os.Chdir(filepath.Join(tmp_src_dir, p.name))
 	os.Setenv("GOPATH", tmp_build_dir + ":" + tmp_import_dir)
 	old_build_number := os.Getenv("BUILD_NUMBER")	
 	os.Setenv("BUILD_NUMBER", p.tag)
@@ -250,11 +246,17 @@ func (p *Package) Build() bool {
 	
 	os.Setenv("BUILD_NUMBER", old_build_number)
 
-	p.logger.Info("Installing", p.name + "-" + p.tag + "...")
+	p.logger.Debug(" * Installing", p.name + "-" + p.tag + "...")
+
+	os.MkdirAll(filepath.Join(p.root, p.tag), 0775)
+	err = FileCopy(tmp_src_dir, filepath.Join(p.root, p.tag, "src"))
+	if err != nil {
+		p.logger.Fatal("Failed to copy source to install folder")
+	}
 
 	err = FileCopy(filepath.Join(tmp_build_dir, "pkg"), filepath.Join(p.root, p.tag, "pkg"))
 	if err != nil {
-		p.logger.Fatal("Failed to copy libraries")
+		p.logger.Fatal("Failed to copy libraries to install folder")
 	}
 
 	err = FileCopy(filepath.Join(tmp_build_dir, "bin"), filepath.Join(p.root, p.tag, "bin"))
@@ -263,6 +265,7 @@ func (p *Package) Build() bool {
 		p.logger.Debug(" * Installed binaries")
 	}
 
+	p.logger.Info("Installed", p.name, p.tag)
 	return true
 }
 
@@ -271,7 +274,10 @@ func (p *Package) Install(tmpdir string) {
 	p.logger.Debug("Starting install of", p.name, p.tag)
 	if p.source == "" {
 		if !p.FindSource() {
-			p.logger.Fatal("ERROR Finding package")
+			if p.tag != "" {
+				p.logger.Fatal("ERROR Couldn't find", p.name, "(" + p.tag + ")", "in any sources")
+			}
+			p.logger.Fatal("ERROR Couldn't find", p.name, "in any sources")
 		}
 	}
 	if !p.Get() {
