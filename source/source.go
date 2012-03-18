2014bb2c94d43d19fbb1eeefaf3defd352972586
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-18 08:07:01 -0700

    Or it will smash the words together

--- source/source.go
+++ source/source.go
@@ -70,11 +70,11 @@ func (s GitSource) Clone(name string, version string, dest string) *SourceError 
 	if version != "" {
 		err := os.Chdir(dest_dir)
 		if err != nil {
-			return NewSourceError(fmt.Sprint("Unable to chdir to checkout version", version, "of", name))
+			return NewSourceError(fmt.Sprintln("Unable to chdir to checkout version", version, "of", name))
 		}
 		_, err = exec.Command("git", "checkout", version).CombinedOutput()
 		if err != nil {
-			return NewSourceError(fmt.Sprint("Invalid version:", version, "of", name, "specified"))
+			return NewSourceError(fmt.Sprintln("Invalid version:", version, "of", name, "specified"))
 		}
 	}
 	return nil
