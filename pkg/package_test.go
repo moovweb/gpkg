a99acdbe08202319c809abbbc9901c4f5cf52c89
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-19 03:39:52 -0700

    Test for the API I'd like

---
+++ pkg/package_test.go
@@ -0,0 +1,22 @@
+package pkg
+
+import "testing"
+
+import . "source"
+
+func TestPackage(t *testing.T) {
+	pkg_path := "/home/jbussdieker/moovweb/gpkg/pkg/testdata"
+	p := NewPackage("simple_cmd", nil, pkg_path, NewLocalSource(pkg_path))
+	err := p.Clone("/tmp/gpkg-test/package-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = p.Clean()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = p.Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
