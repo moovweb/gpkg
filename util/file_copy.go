5199acb694d8cea856693f3746d58e52be29fbfc
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-28 19:39:43 -0700

    Recursive dir copy. Should work on windows.

--- util/file_copy.go
+++ util/file_copy.go
@@ -1,15 +1,56 @@
 package util
 
 import (
-	"errors"
-	"os/exec"
+	"os"
+	"io"
+	"io/ioutil"
+	"path/filepath"
 )
-import "strings"
+
+func copyFile(dst, src string) (int64, error) { 
+        sf, err := os.Open(src) 
+        if err != nil { 
+                return 0, err 
+        } 
+        defer sf.Close() 
+        df, err := os.Create(dst) 
+        if err != nil { 
+                return 0, err 
+        } 
+        defer df.Close() 
+        return io.Copy(df, sf) 
+} 
+
+func copyDir(dst, src string) error {
+	dirs, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			err := os.MkdirAll(filepath.Join(dst, dir.Name()), 0755)
+			if err != nil {
+				return err
+			}
+			err = copyDir(filepath.Join(dst, dir.Name()), filepath.Join(src, dir.Name()))
+			if err != nil {
+				return err
+			}
+		} else {
+			_, err := copyFile(filepath.Join(dst, dir.Name()), filepath.Join(src, dir.Name()))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
 
 func FileCopy(src string, dst string) error {
-	out, err := exec.Command("cp", "-rf", src, dst).CombinedOutput()
+	err := copyDir(dst, src)
 	if err != nil {
-		return errors.New(strings.TrimSpace(string(out)))
+		return err
 	}
 	return nil
 }
